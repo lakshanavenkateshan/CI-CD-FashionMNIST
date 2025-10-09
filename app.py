a25b445cc2e01810bb199c8922c7360ba9f37b1b
lakshanav <vhanah14@gmail.com>
2025-10-09 15:55:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Fashion-MNIST API Running Successfully!"})
+    return jsonify({"message": "Fashion-MNIST API v2 Deployed via CI/CD!"})
 
 @app.route('/predict')
 def predict():
     return jsonify({"prediction": random.choice(["Sneaker", "T-shirt", "Bag", "Coat"])})
 
 if __name__ == '__main__':
+    print("✅ Updated API is live!")  # Added log line
     app.run(host='0.0.0.0', port=5000)
